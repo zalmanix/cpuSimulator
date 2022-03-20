e7a0068bb5b5f8bb23103b9b1a0adb19488579ee
unknown <Maksymilian.Skornog@microsoft.wsei.edu.pl>
2022-03-20 12:38:54 +0100

    inputs and header implementation

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="bg-gray-300 bg-opacity-60 w-full mx-auto text-center h-24 flex justify-center sticky top-0 items-center" style="backdrop-filter: saturate(150%) blur(20px);">
+    <p class="text-center text-2xl text-gray-900">
+      Symulator 
+      <b class="uppercase">intel</b>
+      8086
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,43 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Header/>
+
+    <div class="bg-gray-50">
+      <div 
+        class="flex"
+        v-for="(input, i) in inputs"
+        :key="i"
+      >
+        <span>{{input}}H</span>
+
+        <input class="block w-full text-sm text-slate-500" type="text">
+      </div>
+
+      <div 
+        class="flex"
+        v-for="(input, i) in inputs"
+        :key="i"
+      >
+        <span>{{input}}L</span>
+
+        <input type="text">
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
+import Header from '~/components/Header.vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    Header,
+  },
+  data: () => ({
+   inputs: ['A', 'B', 'C', 'D'],
+  })
 })
-</script>
+
+    Header</script>
