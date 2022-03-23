bb4ceb8c87c25e39c211a37215de02e089861f16
zalmanix <zalmanix@gmail.com>
2022-03-23 15:55:30 +0100

    implementation of buttons

--- pages/index.vue
+++ pages/index.vue
@@ -2,26 +2,66 @@
   <div>
     <Header/>
 
-    <div class="bg-gray-50">
-      <div 
-        class="flex"
-        v-for="(input, i) in inputs"
-        :key="i"
-      >
-        <span>{{input}}H</span>
+    <!-- <div class="bg-green-300"> -->
+    <div>
+      <div class="px-40 flex flex-row">
+        <div class="flex flex-col">
+          <div
+            class="flex p-2"
+            v-for="(input, i) in inputs"
+            :key="i"
+          >
+            <span class="p-1 font-semibold">
+              {{input}}H
+            </span>
 
-        <input class="block w-full text-sm text-slate-500" type="text">
+            <input
+              class="js-input-base w-24 bg-blue-300 focus:bg-blue-400 rounded-lg"
+              type="text"
+              maxlength="4"
+              placeholder="0000"
+            >
+          </div>
+        </div>
+
+        <div class="flex flex-col">
+          <div
+            class="flex p-2"
+            v-for="(input, i) in inputs"
+            :key="i"
+          >
+            <span class="p-1 font-semibold">
+              {{input}}L
+            </span>
+
+            <input
+              class="js-input-base w-24 bg-blue-300 focus:bg-blue-400 rounded-lg text-white"
+              type="text"
+              maxlength="4"
+              placeholder="0000"
+            >
+          </div>
+        </div>
+
+        <div class="p-2 flex flex-col justify-around">
+          <button class="px-4 py-2 font-semibold text-sm bg-green-500 text-white rounded-full shadow-sm hover:bg-green-600">
+            Zapisz
+          </button>
+
+          <button class="px-4 py-2 font-semibold text-sm bg-purple-500 text-white rounded-full shadow-sm hover:bg-purple-600">
+            Randomowe Dane
+          </button>
+
+          <button
+            class="px-4 py-2 font-semibold text-sm bg-red-500 text-white rounded-full shadow-sm hover:bg-red-600"
+            @click="resetValues"
+          >
+            Reset
+          </button>
+        </div>
       </div>
 
-      <div 
-        class="flex"
-        v-for="(input, i) in inputs"
-        :key="i"
-      >
-        <span>{{input}}L</span>
 
-        <input type="text">
-      </div>
     </div>
   </div>
 </template>
@@ -37,7 +77,18 @@ export default Vue.extend({
   },
   data: () => ({
    inputs: ['A', 'B', 'C', 'D'],
-  })
+  }),
+  setup() {
+    function resetValues() {
+      const inputs = document.querySelectorAll('.js-input-base');
+
+      console.log(inputs)
+    }
+
+    return {
+      resetValues
+    }
+  }
 })
 
     Header</script>
